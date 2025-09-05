fbce44ea6029ac4c77482c442f1ca20cb8a9d522
sardor <sardor.info.uz@gmail.com>
2025-09-05 15:06:14 +0500

    aaa

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,303 @@
+////////yandexxxx/////////
+import 'dart:math';
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:yandex_mapkit/yandex_mapkit.dart' as yandex;
+import 'package:perfect_freehand/perfect_freehand.dart' as freehand;
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FilledDrawMapPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class FilledDrawMapPage extends StatefulWidget {
+  const FilledDrawMapPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<FilledDrawMapPage> createState() => _FilledDrawMapPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _FilledDrawMapPageState extends State<FilledDrawMapPage> {
+  yandex.YandexMapController? _controller;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  final yandex.Point _mapCenter =
+  const yandex.Point(latitude: 41.3, longitude: 69.2);
+  double _zoom = 14;
+
+  final List<Stroke> lines = [];
+  Stroke? _currentLine;
+
+  final List<Offset> bubbles = [];
+
+  freehand.StrokeOptions options = freehand.StrokeOptions(
+    size: 4,
+    thinning: 0.7,
+    smoothing: 0.5,
+    streamline: 0.5,
+    simulatePressure: true,
+  );
+
+  bool canDraw = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _generateBubbles(MediaQuery.of(context).size);
     });
   }
 
+  void _generateBubbles(Size size) {
+    final rnd = Random();
+    bubbles.clear();
+    for (int i = 0; i < 500; i++) {
+      bubbles.add(Offset(
+        rnd.nextDouble() * size.width,
+        rnd.nextDouble() * size.height,
+      ));
+    }
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      appBar: AppBar(title: Text("Realtime Draw on Map")),
+      body: Listener(
+        onPointerDown: _onPointerDown,
+        onPointerMove: _onPointerMove,
+        onPointerUp: _onPointerUp,
+        child: Stack(
+          children: [
+            // Xarita
+            yandex.YandexMap(
+              onMapCreated: (controller) async {
+                _controller = controller;
+                await controller.moveCamera(
+                  yandex.CameraUpdate.newCameraPosition(
+                    yandex.CameraPosition(target: _mapCenter, zoom: _zoom),
+                  ),
+                );
+              },
+            ),
+
+            // Faqat chizish holatida gesture layer
+            if (canDraw)
+              Positioned.fill(
+                child: Listener(
+                  onPointerDown: canDraw ? _onPointerDown : null,
+                  onPointerMove: canDraw ? _onPointerMove : null,
+                  onPointerUp: canDraw ? _onPointerUp : null,
+                  behavior: HitTestBehavior.translucent, // touch event pastga o‘tadi
+                ),
+              ),
+
+
+            // Nuqtalar
+            Positioned.fill(
+              child: CustomPaint(
+                painter: BubblePainter(bubbles),
+              ),
+            ),
+
+            // Chizilgan chiziqlar
+            Positioned.fill(
+              child: CustomPaint(
+                painter: StrokePainter(
+                  color: Colors.blue,
+                  lines: [...lines, if (_currentLine != null) _currentLine!],
+                  options: options,
+                ),
+              ),
             ),
           ],
-        ),
+        )
+        ,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: "startButton",
+            onPressed: () {
+              setState(() {
+                canDraw = true;
+                lines.clear();      // eski chiziqlarni o'chiramiz
+                _currentLine = null;
+                bubbles.clear();    // nuqtalarni o'chiramiz
+              });
+            },
+            child: const Icon(Icons.brush),
+            tooltip: "Start Drawing",
+          ),
+
+          const SizedBox(width: 16),
+          FloatingActionButton(
+            heroTag: "clearButton",
+            onPressed: () {
+              setState(() {
+                lines.clear();
+                _currentLine = null;
+                bubbles.clear();
+                _generateBubbles(MediaQuery.of(context).size);
+                canDraw = false;
+              });
+            },
+            child: const Icon(Icons.clear),
+            tooltip: "Clear Drawing",
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onPointerDown(PointerDownEvent details) {
+    if (!canDraw) return;
+    final point =
+    freehand.PointVector(details.localPosition.dx, details.localPosition.dy);
+    setState(() {
+      _currentLine = Stroke([point]);
+      // bubbles.clear();  <-- bu satrni o'chiramiz, shuning uchun nuqtalar yo'qolmaydi
+    });
+  }
+
+
+  void _onPointerMove(PointerMoveEvent details) {
+    if (!canDraw || _currentLine == null) return;
+    final point =
+    freehand.PointVector(details.localPosition.dx, details.localPosition.dy);
+    setState(() {
+      _currentLine =
+          _currentLine!.copyWith(points: [..._currentLine!.points, point]);
+    });
+  }
+
+  void _onPointerUp(PointerUpEvent details) {
+    if (!canDraw || _currentLine == null) return;
+
+    setState(() {
+      final closedLine = _currentLine!.copyWith(closed: true);
+      lines
+        ..clear()
+        ..add(closedLine);
+
+      final path = Path()
+        ..moveTo(closedLine.points.first.dx, closedLine.points.first.dy);
+      for (final p in closedLine.points.skip(1)) {
+        path.lineTo(p.dx, p.dy);
+      }
+      path.close();
+
+      final rnd = Random();
+      final size = MediaQuery.of(context).size;
+
+      final newBubbles = <Offset>[];
+      for (int i = 0; i < 500; i++) {
+        final b = Offset(rnd.nextDouble() * size.width, rnd.nextDouble() * size.height);
+        if (path.contains(b)) newBubbles.add(b);
+      }
+      bubbles
+        ..clear()
+        ..addAll(newBubbles);
+
+      _currentLine = null;
+      canDraw = false;
+    });
+  }
+}
+
+class Stroke {
+  final List<freehand.PointVector> points;
+  final bool closed;
+  const Stroke(this.points, {this.closed = false});
+
+  Stroke copyWith({List<freehand.PointVector>? points, bool? closed}) {
+    return Stroke(
+      points ?? this.points,
+      closed: closed ?? this.closed,
     );
   }
 }
+
+class StrokePainter extends CustomPainter {
+  const StrokePainter({
+    required this.color,
+    required this.lines,
+    required this.options,
+  });
+
+  final Color color;
+  final List<Stroke> lines;
+  final freehand.StrokeOptions options;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final fillPaint = Paint()
+      ..color = Colors.blue.withOpacity(0.2)
+      ..style = PaintingStyle.fill;
+
+    final strokePaint = Paint()
+      ..color = color
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = options.size;
+
+    for (final line in lines) {
+      if (line.points.isEmpty) continue;
+
+      if (line.points.length < 2) {
+        final point = line.points.first;
+        canvas.drawCircle(
+            Offset(point.dx, point.dy), options.size / 2, fillPaint);
+        continue;
+      }
+
+      final path =
+      Path()..moveTo(line.points.first.dx, line.points.first.dy);
+      for (int i = 1; i < line.points.length; i++) {
+        path.lineTo(line.points[i].dx, line.points[i].dy);
+      }
+
+      if (line.closed) {
+        path.close();
+        canvas.drawPath(path, fillPaint);
+        canvas.drawPath(path, strokePaint);
+      } else {
+        canvas.drawPath(path, strokePaint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant StrokePainter oldDelegate) => true;
+}
+
+class BubblePainter extends CustomPainter {
+  final List<Offset> bubbles;
+  BubblePainter(this.bubbles);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final rnd = Random();
+    for (final b in bubbles) {
+      final paint = Paint()
+        ..color =
+        Colors.primaries[rnd.nextInt(Colors.primaries.length)].withOpacity(0.7);
+      canvas.drawCircle(b, 4, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant BubblePainter oldDelegate) => true;
+}
+
+
+
